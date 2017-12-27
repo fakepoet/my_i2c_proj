742168ee68cfc112619c32c3ed620946533da12e
fakepoet <fakepoet0101@gmail.com>
2017-12-28 01:41:06 +0800

    add status_cycle

---
+++ lcd_1602.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+import smbus
+import time
+
+# Define some device parameters
+I2C_ADDR = 0x27  # I2CLCD device address
+LCD_WIDTH = 16   # Maximum characters per line
+
+# Define some device constants
+LCD_CHR = 1  # Mode - Sending data
+LCD_CMD = 0  # Mode - Sending command
+
+LCD_LINE_1 = 0x80  # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0  # LCD RAM address for the 2nd line
+LCD_LINE_3 = 0x94  # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4  # LCD RAM address for the 4th line
+
+LCD_BACKLIGHT = 0x08  # On
+# LCD_BACKLIGHT = 0x00  # Off
+
+ENABLE = 0b00000100  # Enable bit
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+# Cycle constants
+C_DELAY = 5
+
+
+class LCD1206(object):
+    def __init__(self):
+        # Open I2C interface
+        # bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
+        self.bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
+
+    def lcd_init(self):
+        # Initialise display
+        self.lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
+        self.lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
+        self.lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
+        self.lcd_byte(0x0C, LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
+        self.lcd_byte(0x28, LCD_CMD)  # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
+        time.sleep(E_DELAY)
+
+    def lcd_byte(self, bits, mode):
+        # Send byte to data pins
+        # bits = the data
+        # mode = 1 for data
+        #        0 for command
+
+        bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+        bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT
+
+        # High bits
+        self.bus.write_byte(I2C_ADDR, bits_high)
+        self.lcd_toggle_enable(bits_high)
+
+        # Low bits
+        self.bus.write_byte(I2C_ADDR, bits_low)
+        self.lcd_toggle_enable(bits_low)
+
+    def lcd_toggle_enable(self, bits):
+        # Toggle enable
+        time.sleep(E_DELAY)
+        self.bus.write_byte(I2C_ADDR, (bits | ENABLE))
+        time.sleep(E_PULSE)
+        self.bus.write_byte(I2C_ADDR, (bits & ~ENABLE))
+        time.sleep(E_DELAY)
+
+    def lcd_string(self, message, line):
+        # Send string to display
+        for message in message.split('\n'):
+            message = message.ljust(LCD_WIDTH, " ")
+
+            self.lcd_byte(line, LCD_CMD)
+
+            for i in range(LCD_WIDTH):
+                self.lcd_byte(ord(message[i]), LCD_CHR)
+
+    def lcd_clear(self):
+        self.lcd_byte(0x01, LCD_CMD)
+
+    def lcd_status_cycle(self, delay=C_DELAY):
+        from .utils import (get_time_now,
+                            get_ip_info,
+                            get_cpu_temp,
+                            get_cpu_usage,
+                            get_mem_info,)
+        while True:
+            for func in (
+                get_time_now,
+                get_ip_info,
+                get_cpu_temp,
+                get_cpu_usage,
+                get_mem_info,
+            ):
+                msg = func()
+                self.lcd_clear()
+                self.lcd_string(msg)
+                time.sleep(delay)
+
+
+if __name__ == '__main__':
+
+    # Initialise display
+    dev = LCD1206()
+    dev.lcd_init()
+    dev.message('Hello,\n        Master')
+    try:
+        dev.lcd_status_cycle()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        dev.lcd_byte(0x01, LCD_CMD)
---
+++ utils.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+from datetime import datetime
+import commands
+import psutil
+
+
+def get_cpu_temp():
+    cpu_tmp = open('/sys/class/thermal/thermal_zone0/temp')
+    res = cpu_tmp.read()
+    cpu_tmp.close()
+    cpu_tmp = res
+
+    gpu_tmp = commands.getoutput('vcgencmd measure_temp|awk -F= \'{print $2}\'').replace('\'C', '')
+    gpu_tmp = float(gpu_tmp)
+    gpu_temp = 'GPU_TMEP: {:.2f} ˚C'.format(gpu_tmp)
+    cpu_temp = 'CPU_TEMP: {:.2f} ˚C\n'.format(float(cpu_tmp) / 1000)
+    return cpu_temp + gpu_temp
+
+
+def get_time_now():
+    cur = datetime.now().strftime('    %H:%M:%S\n   %Y-%m-%d')
+    return cur
+
+
+def get_ip_info():
+    wlan0_info = psutil.net_if_addrs().get('wlan0', [])
+    ip_info = wlan0_info[0] if wlan0_info else 'NOT FOUND'
+    return 'IP_ADDRESS: \n' + ip_info.address
+
+
+def get_cpu_usage():
+    message = 'CPU_USAGE:\n'
+    per = ' ' * 10 + str(psutil.cpu_percent()) + '%'
+    return message + per
+
+
+def get_mem_info():
+    total = commands.getoutput('free -m|grep Mem:|awk \'{print $2}\'')
+    used = commands.getoutput('free -m|grep Mem:|awk \'{print $3}\'')
+    message = 'MEM USAGE: \n'
+    usage = '{}/{}   {:.2f}%'.format(used, total, float(used) / float(total) * 100)
+    return message + usage
