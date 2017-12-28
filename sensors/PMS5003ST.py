90deb69e625499a471051d17fef6f3dc09106507
fakepoet <fakepoet0101@gmail.com>
2017-12-29 05:09:18 +0800

    fix

--- sensors/PMS5003ST.py
+++ sensors/PMS5003ST.py
@@ -11,27 +11,28 @@ class PMS5003(object):
         ser = serial.Serial("/dev/ttyS0", 9600)
         while True:
             sign1 = ser.read()
-            print '1: %s' % sign1
-            print '1 ord: %s' % ord(sign1)
-            if ord(sign1) == 0x42:
-                sign2 = ser.read()
-                print '2 : %s' % sign2
-                print '2 ord: %s' % ord(sign2)
-                if ord(sign2) == 0x4d:
-                    count = ser.inWaiting()
-                    while count < 38:
-                        time.sleep(0.2)
-                    recv = ser.read(38)
-                    (frame_length, pm1_0_cf, pm2_5_cf, pm10_cf, pm1_0,
-                     pm2_5, pm10, cnt_03, cnt_05, cnt_10, cnt_25, cnt_50, cnt_100,
-                     hcho, temp, humidity, reserve, version, checksum) = struct.unpack('>' + 'H' * 19, recv)
-                    ser.close()
-                    break
-        return dict(
-            pm1_0=pm1_0,
-            pm2_5=pm2_5,
-            pm10=pm10,
-            hcho=float(hcho) / 1000,
-            temp=float(temp) / 10,
-            humidity=(humidity) / 10
-        )
+
+            if ord(sign1) != 0x42:
+                continue
+
+            sign2 = ser.read()
+            if ord(sign2) != 0x4d:
+                continue
+
+            count = ser.inWaiting()
+            while count < 38:
+                time.sleep(0.2)
+                count = ser.inWaiting()
+            recv = ser.read(38)
+            (frame_length, pm1_0_cf, pm2_5_cf, pm10_cf, pm1_0,
+             pm2_5, pm10, cnt_03, cnt_05, cnt_10, cnt_25, cnt_50, cnt_100,
+             hcho, temp, humidity, reserve, version, checksum) = struct.unpack('>' + 'H' * 19, recv)
+            ser.close()
+            return dict(
+                pm1_0=pm1_0,
+                pm2_5=pm2_5,
+                pm10=pm10,
+                hcho=float(hcho) / 1000,
+                temp=float(temp) / 10,
+                humidity=(humidity) / 10
+            )
