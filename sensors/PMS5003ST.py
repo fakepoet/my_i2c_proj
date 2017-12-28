f859d15b33a3c5abc3e967abe8d5bb4ac2e5baff
fakepoet <fakepoet0101@gmail.com>
2017-12-29 05:05:53 +0800

    enchancement

--- sensors/PMS5003ST.py
+++ sensors/PMS5003ST.py
@@ -10,20 +10,23 @@ class PMS5003(object):
     def get_data():
         ser = serial.Serial("/dev/ttyS0", 9600)
         while True:
-            # 获得接收缓冲区字符
-            count = ser.inWaiting()
-            print count
-            if count >= 40:
-                recv = ser.read(40)
-                print recv
-                print type(recv)
-                (sign1, sign2, frame_length, pm1_0_cf, pm2_5_cf, pm10_cf, pm1_0,
-                 pm2_5, pm10, cnt_03, cnt_05, cnt_10, cnt_25, cnt_50, cnt_100,
-                 hcho, temp, humidity, reserve, version, checksum) = struct.unpack('>bb' + 'H' * 19, recv)
-
-                if sign1 == 0x42 and sign2 == 0x4d:
+            sign1 = ser.read()
+            print '1: %s' % sign1
+            print '1 ord: %s' % ord(sign1)
+            if ord(sign1) == 0x42:
+                sign2 = ser.read()
+                print '2 : %s' % sign2
+                print '2 ord: %s' % ord(sign2)
+                if ord(sign2) == 0x4d:
+                    count = ser.inWaiting()
+                    while count < 38:
+                        time.sleep(0.2)
+                    recv = ser.read(38)
+                    (frame_length, pm1_0_cf, pm2_5_cf, pm10_cf, pm1_0,
+                     pm2_5, pm10, cnt_03, cnt_05, cnt_10, cnt_25, cnt_50, cnt_100,
+                     hcho, temp, humidity, reserve, version, checksum) = struct.unpack('>' + 'H' * 19, recv)
+                    ser.close()
                     break
-            time.sleep(1)
         return dict(
             pm1_0=pm1_0,
             pm2_5=pm2_5,
